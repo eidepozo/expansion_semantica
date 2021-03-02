28aeab89733e012347a4799bc4943fa87c8aea6b
eidepozo <eidepozo@gmail.com>
2021-03-02 18:39:25 -0300

    Ajuste busqueda 3 snippets sin publicidad

--- bing_search.py
+++ bing_search.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located
 import time
+from selenium.webdriver.support import expected_conditions as EC
 
 def bing_search(query):
     url = urlunparse(("https", "www.bing.com", "/search", "", urlencode({"q": query}), ""))
@@ -26,8 +27,15 @@ def bing_search_alt(query):
     url = urlunparse(("https", "www.bing.com", "/search", "", urlencode({"q": query}), ""))
     driver = webdriver.Firefox()
     driver.get(url)
-    elements = WebDriverWait(driver,12).until(presence_of_all_elements_located((By.CSS_SELECTOR, 'div.b_caption p')))
+    elements = WebDriverWait(driver,6).until(presence_of_all_elements_located((By.CSS_SELECTOR, 'li.b_algo p')))
     docs = [value.text for value in elements[:3]]
+    while len(docs) != 3:
+        next_page=WebDriverWait(driver, 6).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#b_results > li.b_pag > nav > ul > li:last-child")))
+        next_page.click()
+        elements = WebDriverWait(driver,6).until(presence_of_all_elements_located((By.CSS_SELECTOR, 'li.b_algo p')))
+        for value in elements[:3-len(docs)]:
+            docs.append(value.text) 
     time.sleep(3)
     driver.close()
     return docs    
+ 
