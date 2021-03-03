7934d8d4459dc0538d07a1106ee9eb125112c141
eidepozo <eidepozo@gmail.com>
2021-03-03 19:21:44 -0300

    Ajuste formato vocabulario

--- formatted.py
+++ formatted.py
@@ -13,8 +13,9 @@ def formatted(filtered_data, sentences): # ahora snippet completo o por sentenci
     
     es_sw = stopwords.words('spanish')
     es_sw.extend(['si', 'asi', 'ser', 'tener', 'mas']) 
-    en_sw = stopwords.words('english') 
+    en_sw = stopwords.words('english')
+    en_sw.extend(['www', 'com'])
     for i in range(len(all_words)):
-        all_words[i] = [w for w in all_words[i] if w not in es_sw and w not in en_sw]
+        all_words[i] = [w for w in all_words[i] if w not in es_sw and w not in en_sw and len(w)>1] #filtro adicional
     all_words = [w for w in all_words if w] # filtro para listas vacias
     return all_words
